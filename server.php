737f3e893383900b3abe0c6ef5e3c24498e64582
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-05-06 18:01:15 +0200

    Feat: fix server php

--- server.php
+++ server.php
@@ -1,16 +1,8 @@
 <?php
 $discs_json = file_get_contents ('dischi.json');
 $discs = json_decode($discs_json, true);
-// stampiamo tutti i dischi
-// $response = $discs;
 
-// // richiesta di un disco specifico
-// if ($_GET('discIndex')) {
-//     $discIndex = intval($_GET['discIndex']);
-//     $response = $discs[$discIndex];
-// }
 
 header('Content-Type: application/json');
-
 echo json_encode($discs);
 ?>
