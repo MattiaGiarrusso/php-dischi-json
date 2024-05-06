a517202ea464db48f0e6738621e70e021780c070
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-05-06 17:59:23 +0200

    Feat: create layout and server php

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php
-
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,34 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- cdn link bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Document</title>
-    <!-- cdn axios     -->
+    <title>Dischi Json</title>
+    <!-- cdn axios -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- cdn vue -->
-    <script src="https://unpkg.com/vue@3"></script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
 <body class="bg-secondary">
     <div id="app">
+
         <header class="bg-primary">
             <div class="container">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <h1>SPOTIFY</h1>
             </div>
         </header>
 
         <main>
-            <div class="container mt-5 d-flex flex-wrap">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
+            <div class="container ms-container mt-5 d-flex flex-wrap">
+            
+            <div class="card ms-card" v-for="disc in discs">
+                <img src="" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Title Disc</h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">Artist</h6>
-                    <span class="card-text">Anno</span>
+                    <h5 class="card-title">{{ disc.title }}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">{{ disc.author }}</h6>
+                    <span class="card-text">{{ disc.year }}</span>
                 </div>
             </div>
 
             </div>
-        </main>     
-
+        </main>
 
     </div>
 
--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-        
+            discs:[],        
+        };
+    },
+    methods: {
+        getDiscsFromApi() {
+            axios.get('server.php').
+                then((response) => {
+                    this.discs = response.data;
+                }); 
+        },
+        mounted() {
+            this.getDiscsFromApi();
         }
     }
 }).mount('#app')
